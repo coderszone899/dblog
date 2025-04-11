3b0fe537edc01c6524dfb8213de8df3e28392d44
coderszone899 <lezrel.boro@yahoo.com>
2025-04-11 11:55:35 +0800

    Create package.js

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "dblog",
+  "version": "1.0.0",
+  "private": true,
+  "dependencies": {
+    "ethers": "^6.0.0",
+    "react": "^18.2.0",
+    "react-dom": "^18.2.0",
+    "web3.storage": "^5.1.0"
+  },
+  "scripts": {
+    "start": "react-scripts start",
+    "build": "react-scripts build"
+  },
+  "devDependencies": {
+    "react-scripts": "5.0.1"
+  }
+}
